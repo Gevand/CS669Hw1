3e6b56a817ba05de2fc1591097d6787ba0393452
Gevand <gevand@outlook.com>
2017-09-04 10:52:39 -0700

    R G B Y M C K done.

--- CS669GevandBalayanHomework1/Form1.Designer.cs
+++ CS669GevandBalayanHomework1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CS669GevandBalayanHomework1
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,224 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictInput = new System.Windows.Forms.PictureBox();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnYColorSpace = new System.Windows.Forms.Button();
+            this.btnI = new System.Windows.Forms.Button();
+            this.btnCr = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnCyan = new System.Windows.Forms.Button();
+            this.btnMagenta = new System.Windows.Forms.Button();
+            this.btnBlack = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnQ = new System.Windows.Forms.Button();
+            this.btnCb = new System.Windows.Forms.Button();
+            this.btnV = new System.Windows.Forms.Button();
+            this.btnU = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictInput
+            // 
+            this.pictInput.Location = new System.Drawing.Point(114, 12);
+            this.pictInput.Name = "pictInput";
+            this.pictInput.Size = new System.Drawing.Size(599, 538);
+            this.pictInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictInput.TabIndex = 1;
+            this.pictInput.TabStop = false;
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(12, 67);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(27, 23);
+            this.btnRed.TabIndex = 2;
+            this.btnRed.Text = "R";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(81, 67);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(27, 23);
+            this.btnBlue.TabIndex = 3;
+            this.btnBlue.Text = "B";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(48, 67);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(27, 23);
+            this.btnGreen.TabIndex = 4;
+            this.btnGreen.Text = "G";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // btnYColorSpace
+            // 
+            this.btnYColorSpace.Location = new System.Drawing.Point(12, 96);
+            this.btnYColorSpace.Name = "btnYColorSpace";
+            this.btnYColorSpace.Size = new System.Drawing.Size(96, 23);
+            this.btnYColorSpace.TabIndex = 5;
+            this.btnYColorSpace.Text = "Y (luma)";
+            this.btnYColorSpace.UseVisualStyleBackColor = true;
+            // 
+            // btnI
+            // 
+            this.btnI.Location = new System.Drawing.Point(12, 125);
+            this.btnI.Name = "btnI";
+            this.btnI.Size = new System.Drawing.Size(43, 23);
+            this.btnI.TabIndex = 6;
+            this.btnI.Text = "I";
+            this.btnI.UseVisualStyleBackColor = true;
+            // 
+            // btnCr
+            // 
+            this.btnCr.Location = new System.Drawing.Point(12, 154);
+            this.btnCr.Name = "btnCr";
+            this.btnCr.Size = new System.Drawing.Size(42, 23);
+            this.btnCr.TabIndex = 7;
+            this.btnCr.Text = "Cr";
+            this.btnCr.UseVisualStyleBackColor = true;
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.Location = new System.Drawing.Point(84, 212);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(24, 23);
+            this.btnYellow.TabIndex = 8;
+            this.btnYellow.Text = "Y";
+            this.btnYellow.UseVisualStyleBackColor = true;
+            this.btnYellow.Click += new System.EventHandler(this.btnYellow_Click);
+            // 
+            // btnCyan
+            // 
+            this.btnCyan.Location = new System.Drawing.Point(12, 212);
+            this.btnCyan.Name = "btnCyan";
+            this.btnCyan.Size = new System.Drawing.Size(27, 23);
+            this.btnCyan.TabIndex = 9;
+            this.btnCyan.Text = "C";
+            this.btnCyan.UseVisualStyleBackColor = true;
+            this.btnCyan.Click += new System.EventHandler(this.btnCyan_Click);
+            // 
+            // btnMagenta
+            // 
+            this.btnMagenta.Location = new System.Drawing.Point(48, 212);
+            this.btnMagenta.Name = "btnMagenta";
+            this.btnMagenta.Size = new System.Drawing.Size(27, 23);
+            this.btnMagenta.TabIndex = 10;
+            this.btnMagenta.Text = "M";
+            this.btnMagenta.UseVisualStyleBackColor = true;
+            this.btnMagenta.Click += new System.EventHandler(this.btnMagenta_Click);
+            // 
+            // btnBlack
+            // 
+            this.btnBlack.Location = new System.Drawing.Point(12, 241);
+            this.btnBlack.Name = "btnBlack";
+            this.btnBlack.Size = new System.Drawing.Size(96, 23);
+            this.btnBlack.TabIndex = 11;
+            this.btnBlack.Text = "K";
+            this.btnBlack.UseVisualStyleBackColor = true;
+            this.btnBlack.Click += new System.EventHandler(this.btnBlack_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 23);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnQ
+            // 
+            this.btnQ.Location = new System.Drawing.Point(65, 125);
+            this.btnQ.Name = "btnQ";
+            this.btnQ.Size = new System.Drawing.Size(43, 23);
+            this.btnQ.TabIndex = 13;
+            this.btnQ.Text = "Q";
+            this.btnQ.UseVisualStyleBackColor = true;
+            // 
+            // btnCb
+            // 
+            this.btnCb.Location = new System.Drawing.Point(65, 154);
+            this.btnCb.Name = "btnCb";
+            this.btnCb.Size = new System.Drawing.Size(42, 23);
+            this.btnCb.TabIndex = 14;
+            this.btnCb.Text = "Cb";
+            this.btnCb.UseVisualStyleBackColor = true;
+            // 
+            // btnV
+            // 
+            this.btnV.Location = new System.Drawing.Point(65, 183);
+            this.btnV.Name = "btnV";
+            this.btnV.Size = new System.Drawing.Size(42, 23);
+            this.btnV.TabIndex = 16;
+            this.btnV.Text = "V";
+            this.btnV.UseVisualStyleBackColor = true;
+            // 
+            // btnU
+            // 
+            this.btnU.Location = new System.Drawing.Point(12, 183);
+            this.btnU.Name = "btnU";
+            this.btnU.Size = new System.Drawing.Size(42, 23);
+            this.btnU.TabIndex = 15;
+            this.btnU.Text = "U";
+            this.btnU.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1134, 861);
+            this.Controls.Add(this.btnV);
+            this.Controls.Add(this.btnU);
+            this.Controls.Add(this.btnCb);
+            this.Controls.Add(this.btnQ);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBlack);
+            this.Controls.Add(this.btnMagenta);
+            this.Controls.Add(this.btnCyan);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnCr);
+            this.Controls.Add(this.btnI);
+            this.Controls.Add(this.btnYColorSpace);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.pictInput);
+            this.Name = "frmMain";
+            this.Text = "Gevand Balayan Homework 1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictInput;
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnYColorSpace;
+        private System.Windows.Forms.Button btnI;
+        private System.Windows.Forms.Button btnCr;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnCyan;
+        private System.Windows.Forms.Button btnMagenta;
+        private System.Windows.Forms.Button btnBlack;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnQ;
+        private System.Windows.Forms.Button btnCb;
+        private System.Windows.Forms.Button btnV;
+        private System.Windows.Forms.Button btnU;
     }
 }
 
--- CS669GevandBalayanHomework1/Form1.cs
+++ CS669GevandBalayanHomework1/Form1.cs
@@ -10,11 +10,177 @@ using System.Windows.Forms;
 
 namespace CS669GevandBalayanHomework1
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        private bool imageLoaded = false;
+        public frmMain()
         {
             InitializeComponent();
+            InitButtons();
+        }
+
+        private enum ColorCode { R, G, B, Yluma, I, Q, Cr, Cb, U, V, C, M, Y, K }
+        private void InitButtons()
+        {
+            btnRed.Enabled = imageLoaded;
+            btnBlue.Enabled = imageLoaded;
+            btnGreen.Enabled = imageLoaded;
+
+
+            btnCyan.Enabled = imageLoaded;
+            btnYellow.Enabled = imageLoaded;
+            btnMagenta.Enabled = imageLoaded;
+            btnBlack.Enabled = imageLoaded;
+        }
+        private Bitmap GenerateBitmap(ColorCode input)
+        {
+            Bitmap returnedBitmap = new Bitmap(pictInput.Image);
+            switch (input)
+            {
+                case ColorCode.R:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            var newPixel = Color.FromArgb(pixel.R, pixel.R, pixel.R);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.G:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            var newPixel = Color.FromArgb(pixel.G, pixel.G, pixel.G);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.B:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            var newPixel = Color.FromArgb(pixel.B, pixel.B, pixel.B);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.C:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            int black = Math.Min(Math.Min(255 - pixel.R, 255 - pixel.B), 255 - pixel.G);
+                            int cyan = (255 - pixel.R - black) % (255 - black);
+                            var newPixel = Color.FromArgb(cyan, cyan, cyan);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.M:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            int black = Math.Min(Math.Min(255 - pixel.R, 255 - pixel.B), 255 - pixel.G);
+                            int magenta = (255 - pixel.G - black) % (255 - black);
+                            var newPixel = Color.FromArgb(magenta, magenta, magenta);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.Y:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            int black = Math.Min(Math.Min(255 - pixel.R, 255 - pixel.B), 255 - pixel.G);
+                            int yellow = (255 - pixel.B - black) % (255 - black);
+                            var newPixel = Color.FromArgb(yellow, yellow, yellow);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+                case ColorCode.K:
+                    for (int i = 0; i < returnedBitmap.Width; i++)
+                        for (int j = 0; j < returnedBitmap.Height; j++)
+                        {
+                            var pixel = returnedBitmap.GetPixel(i, j);
+                            int black = Math.Min(Math.Min(255 - pixel.R, 255 - pixel.B), 255 - pixel.G);
+                            var newPixel = Color.FromArgb(black, black, black);
+                            returnedBitmap.SetPixel(i, j, newPixel);
+                        }
+                    break;
+
+            }
+            return returnedBitmap;
+        }
+        private void GenerateNewForm(Bitmap bmp)
+        {
+            Form tempForm = new Form();
+            tempForm.Height = this.Height;
+            tempForm.Width = this.Width;
+            PictureBox tempBox = new PictureBox();
+            tempBox.Image = bmp;
+            tempBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            tempForm.Controls.Add(tempBox);
+            tempForm.Show();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                using (OpenFileDialog dlg = new OpenFileDialog())
+                {
+                    dlg.Title = "Open Image";
+                    dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        pictInput.Image = new Bitmap(dlg.FileName);
+                        imageLoaded = true;
+                        InitButtons();
+                    }
+                }
+            }
+            catch
+            {
+                imageLoaded = false;
+                InitButtons();
+                MessageBox.Show("Error! Couldn't load image.");
+            }
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.R));
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.G));
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.B));
+        }
+
+        private void btnCyan_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.C));
+        }
+
+        private void btnMagenta_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.M));
+        }
+
+        private void btnYellow_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.Y));
+        }
+
+        private void btnBlack_Click(object sender, EventArgs e)
+        {
+            GenerateNewForm(GenerateBitmap(ColorCode.K));
         }
     }
 }
